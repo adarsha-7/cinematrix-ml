e0c9fe1672fcf315bd1b56081a88024cab0e38dd
Adarsha Ghimire <adarshghimire2005@gmail.com>
2026-01-19 21:50:15 +0545

    setup fastapi server for recommendation ml microservice

---
+++ api/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from api import recommend
+
+app = FastAPI()
+
+
+app.include_router(recommend.router, prefix="/recommend", tags=["recommend"])
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Cinematrix movie recommendation API."}
