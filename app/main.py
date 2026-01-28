f3cd0f7f3dac6708bdf21d26d47f2d07fcb64c5f
Adarsha Ghimire <adarshghimire2005@gmail.com>
2026-01-28 15:47:46 +0545

    added a root endpoint

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ app = FastAPI()
 
 app.include_router(recommend.router, prefix="/recommend", tags=["recommend"])
 
+@app.head("/")
+async def root():
+    return {"message": "Welcome to Cinematrix movie recommendation API."}
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to Cinematrix movie recommendation API."}
